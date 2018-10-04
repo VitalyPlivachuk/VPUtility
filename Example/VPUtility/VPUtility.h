d20b59e6c84dd9e10cfad78044c611d5ac1d3819
Vitaly Plivachuk <>
2018-10-04 14:27:55 +0300

    + Extesions

---
+++ Example/VPUtility/VPUtility.h
@@ -0,0 +1,19 @@
+//
+//  VPUtility.h
+//  VPUtility
+//
+//  Created by admin on 10/4/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VPUtility.
+FOUNDATION_EXPORT double VPUtilityVersionNumber;
+
+//! Project version string for VPUtility.
+FOUNDATION_EXPORT const unsigned char VPUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPUtility/PublicHeader.h>
+
+
--- VPUtility.podspec
+++ VPUtility.podspec
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'VPUtility/Classes/**/*'
+  s.source_files = 'VPUtility/*'
   
   # s.resource_bundles = {
   #   'VPUtility' => ['VPUtility/Assets/*.png']
